66193be91a2e7d823b1fd41de47b0abf623ea343
Cynthia Chibana <chibana.cynthia@gmail.com>
2016-04-20 02:57:22 -0300

    Agrego un ejemplo para las conexiones - Proyecto provisorio

---
+++ ejemplo_conexiones/src/mainConexiones.c
@@ -0,0 +1,88 @@
+/*
+ * Ejemplo de conexiones del lado del servidor
+ *
+ * Para conectarse como cliente, desde la terminar correr: telnet 127.0.0.1 9999
+ * Nota: para salir de telnet, presionar Ctrl + ]
+ * TODO: Cuando lo use para los procesos, agregar logs para cuando se establecen las conexiones
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PUERTO "9999"
+#define BACKLOG 5			// Define cuantas conexiones vamos a mantener pendientes al mismo tiempo
+#define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
+
+int main()
+{
+    struct addrinfo hints, *serverInfo;
+    int result_getaddrinfo;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; 		// AF_INET or AF_INET6 to force version
+	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
+	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
+
+	getaddrinfo(NULL, PUERTO, &hints, &serverInfo); // Notar que le pasamos NULL como IP, ya que le indicamos que use localhost en AI_PASSIVE
+
+    //getaddrinfo() - Obtiene los datos de la direccion de red actual y lo guarda en serverInfo.
+    if ((result_getaddrinfo = getaddrinfo(NULL, PUERTO, &hints, &serverInfo)) != 0) {
+        fprintf(stderr, "error: getaddrinfo: %s\n", gai_strerror(result_getaddrinfo));
+        return 2;
+    }
+
+	//Para saber cuales son las conexiones que se quieren establecer conmigo.
+	//Mediante socket(), obtengo el File Descriptor que me proporciona el sistema (un integer identificador).
+    // Obtengo un socket para que escuche las conexiones entrantes
+	int listenningSocket;
+	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+
+    printf("Asocio al listennigSocket al puerto: %s.\n", PUERTO);
+
+	//Asocio al socket con el puerto para escuchar las conexiones en dicho puerto
+	bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
+	freeaddrinfo(serverInfo); // Libero serverInfo
+
+    printf("Listening....\n");
+
+	//Le digo al socket que se quede escuchando
+	listen(listenningSocket, BACKLOG);		// IMPORTANTE: listen() es una syscall BLOQUEANTE.
+
+	//Si hay una conexion entrante, la acepta y crea un nuevo socket mediante el cual nos podamos comunicar con el cliente
+	//El listennigSocket lo seguimos usando para escuchar las conexiones entrantes.
+	//Nota TODO: Para que el listenningSocket vuelva a esperar conexiones, necesitariamos volver a decirle que escuche, con listen();
+
+	struct sockaddr_in addr; // Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
+	socklen_t addrlen = sizeof(addr);
+
+	int socketCliente = accept(listenningSocket, (struct sockaddr *) &addr, &addrlen);
+
+	//Ya estamos listos para recibir paquetes de nuestro cliente...
+	//Vamos a ESPERAR (ergo, funcion bloqueante) que nos manden los paquetes, y los imprimiremos por pantalla.
+
+	char package[PACKAGESIZE];
+	int status = 1;		// Estructura que maneja el status de los recieve.
+
+	printf("Cliente conectado. Esperando mensajes:\n");
+
+	//Cuando el cliente cierra la conexion, recv() devolvera 0.
+	while (status != 0){
+	    memset (package,'\0',PACKAGESIZE); //Lleno de '\0' el package, para que no me muestre basura
+		status = recv(socketCliente, (void*) package, PACKAGESIZE, 0);
+		if (status != 0) printf("%s", package);
+	}
+
+	printf("Cliente se ha desconectado. Finalizo todas las conexiones.\n");
+
+	//Al terminar el intercambio de paquetes, cerramos todas las conexiones.
+	close(socketCliente);
+	close(listenningSocket);
+
+	return 0;
+}
+
