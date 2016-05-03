e898ff51a1540c226c88b2e391bf8f35de3e754e
Cynthia Chibana <chibana.cynthia@gmail.com>
2016-05-03 20:53:18 -0300

    [PARSER] Analiza instruccion, pero no pude obtener cada instruccion del metadata

---
+++ ejemplo_parser_ansisop/src/ejemplo_parser.c
@@ -0,0 +1,107 @@
+/*
+ * ejemplo_parser.c
+ *
+ *  Created on: 1/5/2016
+ *      Author: utnso
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "commons/config.h"
+#include "commons/log.h"
+#include "commons/string.h"
+#include "parser/parser.h"
+#include "parser/metadata_program.h"
+
+#define PACKAGESIZE 1024
+
+t_log* logger;
+
+//Ejemplo para el parser
+
+static const int CONTENIDO_VARIABLE = 20;
+static const int POSICION_MEMORIA = 0x10;
+
+t_puntero dummy_definirVariable(t_nombre_variable variable) {
+	printf("definir la variable %c\n", variable);
+	return POSICION_MEMORIA;
+}
+
+t_puntero dummy_obtenerPosicionVariable(t_nombre_variable variable) {
+	printf("Obtener posicion de %c\n", variable);
+	return POSICION_MEMORIA;
+}
+
+t_valor_variable dummy_dereferenciar(t_puntero puntero) {
+	printf("Dereferenciar %d y su valor es: %d\n", puntero, CONTENIDO_VARIABLE);
+	return CONTENIDO_VARIABLE;
+}
+
+void dummy_asignar(t_puntero puntero, t_valor_variable variable) {
+	printf("Asignando en %d el valor %d\n", puntero, variable);
+}
+
+void dummy_imprimir(t_valor_variable valor) {
+	printf("Imprimir %d\n", valor);
+}
+
+void dummy_imprimirTexto(char* texto) {
+	printf("ImprimirTexto: %s", texto);
+}
+
+AnSISOP_funciones functions = {
+		.AnSISOP_definirVariable		= dummy_definirVariable,
+		.AnSISOP_obtenerPosicionVariable= dummy_obtenerPosicionVariable,
+		.AnSISOP_dereferenciar			= dummy_dereferenciar,
+		.AnSISOP_asignar				= dummy_asignar,
+		.AnSISOP_imprimir				= dummy_imprimir,
+		.AnSISOP_imprimirTexto			= dummy_imprimirTexto,
+
+};
+
+AnSISOP_kernel kernel_functions = { };
+
+
+int main(int argc,char *argv[]) {
+
+	logger = log_create("parser.log", "PARSER",true, LOG_LEVEL_INFO);
+
+	log_info(logger, "Inciando proceso ejemplo de parser..");
+	//Primero pruebo levantar y analizar el codigo de un archivo
+
+	//Levanto un archivo ansisop de prueba
+	char* line;
+
+	line = "begin\nvariables a, b\na = 3\nb = 5\na = b + 12\nend\n";
+
+
+	printf("Ejecutando AnalizadorLinea\n");
+    analizadorLinea("a = 3", &functions, &kernel_functions);
+	printf("================\n");
+
+	printf("================\n");
+	printf("El programa de ejemplo es %s\n", line);
+	printf("================\n");
+	analizadorLinea(strdup(line), &functions, &kernel_functions);
+	printf("================\n");
+
+	t_metadata_program* metadata;
+	metadata = metadata_desde_literal(line);
+
+	int start = metadata->instrucciones_serializado->start;
+	int size = metadata->instrucciones_serializado->offset;
+
+   char dest[50];
+   printf("Before memcpy dest = %s\n", dest);
+   memcpy(dest, &metadata->instrucciones_serializado->start, metadata->instrucciones_serializado->offset +1);
+   printf("After memcpy dest = %s\n", dest);
+
+   printf("================\n");
+
+   t_puntero_instruccion instrucciones = metadata->instruccion_inicio;
+   analizadorLinea(&instrucciones, &functions, &kernel_functions);
+
+	log_destroy(logger);
+
+	return EXIT_SUCCESS;
+}
