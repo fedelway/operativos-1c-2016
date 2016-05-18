4df468ef15f6a6117602578d95a575528daff8a0
Cynthia Chibana <chibana.cynthia@gmail.com>
2016-05-18 02:53:21 -0300

    [PRIMITIVAS ANSISOP][CPU]Cambio definición de funciones correspondientes a las primitivas ansisop

--- cpu/src/primitivasAnsisop.c
+++ cpu/src/primitivasAnsisop.c
@@ -10,65 +10,97 @@
 static const int CONTENIDO_VARIABLE = 20;
 static const int POSICION_MEMORIA = 0x10;
 
+//t_posicion socketes_definirVariable(t_nombre_variable identificador_variable);
 t_puntero socketes_definirVariable(t_nombre_variable variable) {
 	printf("Definir la variable %c\n", variable);
 	return POSICION_MEMORIA;
 }
 
+//TODO: Cambiar t_puntero por t_posicion
+
+//t_posicion socketes_obtenerPosicionVariable(t_nombre_variable identificador_variable);
 t_puntero socketes_obtenerPosicionVariable(t_nombre_variable variable) {
 	printf("Obtener posicion de %c\n", variable);
 	return POSICION_MEMORIA;
 }
 
+//t_valor_variable socketes_dereferenciar(t_posicion direccion_variable);
 t_valor_variable socketes_dereferenciar(t_puntero puntero) {
 	printf("Dereferenciar %d y su valor es: %d\n", puntero, CONTENIDO_VARIABLE);
 	return CONTENIDO_VARIABLE;
 }
-
+//void socketes_asignar(t_posicion direccion_variable, t_valor_variable valor)
 void socketes_asignar(t_puntero puntero, t_valor_variable variable) {
 	printf("Asignando en %d el valor %d\n", puntero, variable);
 }
 
 //TODO: Cambiar parámetros que recibe y devuelve!
 
-void socketes_obtenerValorCompartida(){
+//t_valor_variable socketes_obtenerValorCompartida(t_nombre_compartida variable);
+t_valor_variable socketes_obtenerValorCompartida(t_nombre_compartida variable){
+	t_valor_variable valor_variable;
 	printf("Obtener Valor Compartida\n");
+	return valor_variable;
 }
 
-void socketes_adignarValorCompartida(){
+//t_valor_variable socketes_asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor)
+t_valor_variable socketes_adignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor){
+	t_valor_variable valor_variable;
 	printf("Asignar Valor Compartida\n");
+	return valor_variable;
 }
-
-void socketes_irAlLabel(){
+//t_puntero_instruccion socketes_irAlLabel(t_nombre_etiqueta etiqueta);
+t_puntero_instruccion socketes_irAlLabel(t_nombre_etiqueta etiqueta){
+	t_puntero_instruccion puntero_instruccion;
 	printf("Ir al Label\n");
+	return puntero_instruccion;
 }
-
-void socketes_llamarFuncion(){
-	printf("Llamar función:\n");
+//void socketes_llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar)
+void socketes_llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar){
+	printf("Llamar con retorno:\n");
 }
-
-void socketes_retornar(){
+//t_puntero_instruccion socketes_retornar(t_valor_variable retorno);
+t_puntero_instruccion socketes_retornar(t_valor_variable retorno){
+	t_puntero_instruccion puntero_instruccion;
 	printf("Retornar\n");
+	return puntero_instruccion;
 }
 
-void socketes_imprimir(t_valor_variable valor) {
-	printf("Imprimir %d\n", valor);
+//int socketes_imprimir(t_valor_variable valor_mostrar);
+int socketes_imprimir(t_valor_variable valor_mostrar) {
+	int nro_entero = 17;
+	printf("Imprimir %d\n", valor_mostrar);
+	return nro_entero;
 }
 
-void socketes_imprimirTexto(char* texto) {
+//int socketes_imprimirTexto(char* texto);
+int socketes_imprimirTexto(char* texto) {
+	int nro_entero = 17;
 	printf("ImprimirTexto: %s", texto);
+	return nro_entero;
 }
 
 //TODO: Modificar funciones!!
-void socketes_entradaSalida(){
+
+//int socketes_entradaSalida(t_nombre_dispositivo dispositivo, int tiempo);
+int socketes_entradaSalida(t_nombre_dispositivo dispositivo, int tiempo){
+	int nro_entero = 17;
 	printf("Entrada Salida\n");
+	return nro_entero;
 }
 
-void socketes_wait(){
+//int socketes_wait(t_nombre_semaforo identificador_semaforo);
+int socketes_wait(t_nombre_semaforo identificador_semaforo){
+	int nro_entero = 17;
 	printf("Wait \n");
+	return nro_entero;
 }
 
-void socketes_signal(){
+//int socketes_signal(t_nombre_semaforo identificador_semaforo);
+int socketes_signal(t_nombre_semaforo identificador_semaforo){
+	int nro_entero = 17;
 	printf("Signal \n");
+
+	return nro_entero;
 }
 
--- cpu/src/primitivasAnsisop.h
+++ cpu/src/primitivasAnsisop.h
@@ -14,15 +14,16 @@ t_puntero socketes_definirVariable(t_nombre_variable variable);
 t_puntero socketes_obtenerPosicionVariable(t_nombre_variable variable);
 t_valor_variable socketes_dereferenciar(t_puntero puntero);
 void socketes_asignar(t_puntero puntero, t_valor_variable variable);
-void socketes_obtenerValorCompartida();
-void socketes_adignarValorCompartida();
-void socketes_irAlLabel();
-void socketes_llamarFuncion();
-void socketes_retornar();
-void socketes_imprimir(t_valor_variable valor);
-void socketes_imprimirTexto(char* texto);
-void socketes_entradaSalida();
-void socketes_wait();
-void socketes_signal();
+t_valor_variable socketes_obtenerValorCompartida(t_nombre_compartida variable);
+t_valor_variable socketes_adignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor);
+t_puntero_instruccion socketes_irAlLabel(t_nombre_etiqueta etiqueta);
+void socketes_llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
+t_puntero socketes_retornar(t_valor_variable retorno);
+int socketes_imprimir(t_valor_variable valor_mostrar);
+int socketes_imprimirTexto(char* texto);
+int socketes_entradaSalida(t_nombre_dispositivo dispositivo, int tiempo);
+int socketes_wait(t_nombre_semaforo identificador_semaforo);
+int socketes_signal(t_nombre_semaforo identificador_semaforo);
+
 
 #endif /* PRIMITIVASANSISOP_H_ */
